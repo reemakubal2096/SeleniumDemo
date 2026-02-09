2a3d5299214c0baff73fcae913346acedc852a68
Reema Kubal <reemakubal2096@gmail.com>
2026-02-09 11:40:51 +0530

    files added

---
+++ TstNGProjects/src/projects/basics.java
@@ -0,0 +1,18 @@
+package projects;
+
+import org.testng.annotations.Test;
+
+public class basics {
+	
+	@Test
+	public void demo01()
+	{
+		System.out.println("Hellaaa");
+	}
+
+	@Test
+	public void demo02()
+	{
+		System.out.println("Byee");
+	}
+}
---
+++ TstNGProjects/src/projects/loginmodule.java
@@ -0,0 +1,17 @@
+package projects;
+
+import org.testng.annotations.Test;
+
+public class loginmodule {
+	
+	@Test
+	public void login()
+	{
+		
+	}
+	
+	
+	@Test
+	public void 
+
+}
